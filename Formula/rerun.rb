f970f6b08a5763cb7b23c9364a3fe097bbac46f6
William Hayes Barber <barberhayes@gmail.com>
2026-03-28 16:57:05 -0400

    update to v1.2.0 (#18)

--- Formula/rerun.rb
+++ Formula/rerun.rb
@@ -1,8 +1,8 @@
 class Rerun < Formula
   desc "Watch a directory for file changes and automatically restart a command"
   homepage "https://github.com/HayesBarber/rerun"
-  url "https://api.github.com/repos/HayesBarber/rerun/tarball/v1.1.0"
-  sha256 "b2c7d8a540586a39da46a2653608079b44c0398fc4a14a6f6840ba110dbaa5cc"
+  url "https://api.github.com/repos/HayesBarber/rerun/tarball/v1.2.0"
+  sha256 "6b9a2d927d8f3f05db3134b93f17e0e4b6b93c99fc04df4aa0e23c9c27a5272e"
   license "MIT"
 
   depends_on "rust" => :build
