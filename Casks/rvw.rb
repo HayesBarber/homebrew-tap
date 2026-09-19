3dccfef3b80b72c8580df606c5f3512b86fcce09
Hayes Barber <barberhayes@gmail.com>
2026-09-19 11:23:39 -0400

    update rvw version

--- Casks/rvw.rb
+++ Casks/rvw.rb
@@ -1,6 +1,6 @@
 cask "rvw" do
-  version "1.0.0-alpha.6"
-  sha256 "31eb9aca397179622854b89ceecf1c819779f2844b1c1068c7cbcdd80a0d3c8a"
+  version "1.0.0-alpha.7"
+  sha256 "8971d690405ac04ede23c1657f0975f93b2c2f0b82e977b77c58868220393953"
 
   url "https://github.com/HayesBarber/rvw/releases/download/v#{version}/Rvw-#{version}.zip"
 
